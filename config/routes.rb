51bca1d232b997d7948b94916f662d0a834184c0
KayKawa <goodenoughliving1221@gmail.com>
2022-06-28 14:12:02 +0900

    ユーザー情報表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/edit'
   devise_for :users
   get 'messages/index'
+  resources :users ,only: [:edit,:update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root to:"messages#index"
   # Defines the root path route ("/")
